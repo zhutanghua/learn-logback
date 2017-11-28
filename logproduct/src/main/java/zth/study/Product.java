2f11de09f87be7ada9f74788d411661e161c8ce7
zhutanghua <tanghua.zhu02@gmail.com>
2017-11-28 17:55:31 +0800

    logbackdemo

---
+++ logproduct/src/main/java/zth/study/Product.java
@@ -0,0 +1,12 @@
+package zth.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Product {
+	public static final Logger logger = LoggerFactory.getLogger(Product.class);
+
+	public static void main(String[] args) {
+		logger.info("聊得来的聊得来的的得觉得");
+	}
+}
